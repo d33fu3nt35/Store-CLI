939cb916070e4b02360923fc84aadce0dd6ad4e3
Robin Ury <r.ury@tuta.io>
2017-05-15 16:48:55 -0500

    Refactored body of class into separate methods

--- src/StoreClass.java
+++ src/StoreClass.java
@@ -1,12 +1,38 @@
-import javax.sound.midi.SysexMessage;
 import java.text.NumberFormat;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
-/**
- * Created by daniel on 5/15/17.
- */
 public class StoreClass {
+    public static void printReceipt(String receipt, double total) {
+        int counter = 1;
+        for (int i = 0; i < receipt.length() - 1; ) {
+            String separator = "\t";
+            int start = receipt.indexOf(",", i);
+            i = receipt.indexOf(",", start + 1); //end
+            counter++;
+            if (counter == 4) {
+                counter = 1;
+                separator = "\n";
+            }
+            System.out.printf(receipt.substring(start + 1, i) + separator);
+        }
+        NumberFormat in = NumberFormat.getCurrencyInstance();
+        System.out.println("Your Total:\t" + in.format(total));
+    }
+
+    public static String getItem(Scanner optionScan) {
+        System.out.println("Enter Item Name.");
+        String itemName = optionScan.next();
+
+        System.out.println("Enter Item Price");
+        double itemPrice = optionScan.nextDouble();
+
+        System.out.println("Enter Item Quantity");
+        int itemQuantity = optionScan.nextInt();
+
+        return itemName + "," + itemPrice + "," + itemQuantity;
+
+
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double total = 0.0;
@@ -17,60 +43,27 @@ public class StoreClass {
             System.out.println("1. Sale. \n 2. Print Receipt \n 3. Exit");
             Scanner optionScan = new Scanner(System.in);
             int input = optionScan.nextInt();
-            if (input == 1){
-                while(true){
+            switch (input) {
+                case 1:
+                    do {
+                        String newItem = getItem(optionScan);
+                        // Parse strings returned by getItem to calculate total
+                        String newPrice = newItem.substring(newItem.indexOf(",") + 1, newItem.lastIndexOf(","));
+                        String newQty = newItem.substring(newItem.lastIndexOf(",") + 1);
+                        total += Double.parseDouble(newPrice) * Integer.parseInt(newQty);
 
-                    System.out.println("Enter Item Name.");
-                    String itemName = optionScan.next();
-
-                    System.out.println("Enter Item Price");
-                    double itemPrice = optionScan.nextDouble();
-
-                    System.out.println("Enter Item Quantity");
-                    int itemQuanity = optionScan.nextInt();
-
-                    total += itemPrice * itemQuanity;
-                    receipt += itemName + "," + itemPrice + "," + itemQuanity + ",";
-
-                    System.out.println("Do you wish to continue? Y/n:");
-                    String cont = optionScan.next();
-                    if (!cont.toLowerCase().equals("y")) {
-                        System.out.println(receipt.length());
-                        break;
-                    }
-                }
-
-            }else if (input == 2){
-                int counter = 1;
-                for (int i =0; i < receipt.length()-1;) {
-                    String seperate = "\t";
-                    int start = receipt.indexOf(",", i);
-                    i = receipt.indexOf(",", start+1); //end
-                    counter++;
-                    if(counter == 4){
-                        counter = 1;
-                        seperate = "\n";
-                    }
-                    System.out.printf(receipt.substring(start+1, i) + seperate);
-
-                }
-                NumberFormat in=NumberFormat.getCurrencyInstance();
-                System.out.println("Your Total:" + "\t" + in.format(total));
-
-            }else {
-                System.exit(0);
+                        System.out.println("Do you wish to continue? Y/n:");
+                        String cont = optionScan.next();
+                        if (!cont.toLowerCase().equals("y")) {
+                            System.out.println(receipt.length());
+                            break;
+                        }
+                    } while (true);
+                case 2:
+                    printReceipt(receipt, total);
+                default:
+                    System.exit(0);
             }
-
-
-            // MENU
-            // EVALUATE INPUT MENU
-            // SALE LOOP
-            // TOTAL = TOTAL + SALE.PRICE + SALE QUANTITY
-            // RECEIPT = RECEIPT + SALENAME + ' , ' + SALEPRICE + ' , ' + SALEQUANTITY + ' , '
-            // PRINT
-            // ITERATE THROUGH STRING
-
-
         }while(true);
 
     }
